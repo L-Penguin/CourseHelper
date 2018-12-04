250df0760a3811ad5a5b472b94f77b8b729a2361
L-Penguin <jiangjize2000@gmail.com>
2018-12-03 22:49:35 -0600

    add graphview

---
+++ app/src/main/java/com/tiancai/app/coursehelper/GraphActivity.java
@@ -0,0 +1,156 @@
+package com.tiancai.app.coursehelper;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
+
+import de.blox.graphview.Node;
+import de.blox.graphview.BaseGraphAdapter;
+import de.blox.graphview.Graph;
+import de.blox.graphview.GraphAdapter;
+import de.blox.graphview.GraphView;
+import de.blox.graphview.tree.BuchheimWalkerAlgorithm;
+import de.blox.graphview.tree.BuchheimWalkerConfiguration;
+
+public class GraphActivity extends AppCompatActivity {
+    private int nodeCount = 1;
+    private Node currentNode;
+    protected BaseGraphAdapter<ViewHolder> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graph);
+
+        final Graph graph = createGraph();
+        setupFab(graph);
+        setupAdapter(graph);
+        setupToolbar();
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            ab.setHomeAsUpIndicator(R.drawable.ic_arrow_back);
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    private void setupAdapter(Graph graph) {
+        final GraphView graphView = findViewById(R.id.graph);
+
+        adapter = new BaseGraphAdapter<ViewHolder>(this, R.layout.node, graph) {
+            @NonNull
+            @Override
+            public ViewHolder onCreateViewHolder(View view) {
+                return new ViewHolder(view);
+            }
+
+            @Override
+            public void onBindViewHolder(ViewHolder viewHolder, Object data, int position) {
+                viewHolder.textView.setText(data.toString());
+            }
+        };
+
+        setAlgorithm(adapter);
+
+        graphView.setAdapter(adapter);
+        graphView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                currentNode = adapter.getNode(position);
+                Snackbar.make(graphView, "Clicked on " + currentNode.getData().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void setupFab(final Graph graph) {
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        final BuchheimWalkerConfiguration.Builder builder = new BuchheimWalkerConfiguration.Builder()
+                .setSiblingSeparation(100)
+                .setLevelSeparation(300)
+                .setSubtreeSeparation(300);
+        builder.setOrientation(BuchheimWalkerConfiguration.ORIENTATION_TOP_BOTTOM);
+        adapter.setAlgorithm(new BuchheimWalkerAlgorithm(builder.build()));
+        adapter.notifyInvalidated();
+        return true;
+    }
+
+    public Graph createGraph() {
+        final Graph graph = new Graph();
+        final Node node1 = new Node(getNodeText());
+        final Node node2 = new Node(getNodeText());
+        final Node node3 = new Node(getNodeText());
+        final Node node4 = new Node(getNodeText());
+        final Node node5 = new Node(getNodeText());
+        final Node node6 = new Node(getNodeText());
+        final Node node8 = new Node(getNodeText());
+        final Node node7 = new Node(getNodeText());
+        final Node node9 = new Node(getNodeText());
+        final Node node10 = new Node(getNodeText());
+        final Node node11 = new Node(getNodeText());
+        final Node node12 = new Node(getNodeText());
+
+        graph.addEdge(node1, node2);
+        graph.addEdge(node1, node3);
+        graph.addEdge(node1, node4);
+        graph.addEdge(node2, node5);
+        graph.addEdge(node2, node6);
+        graph.addEdge(node6, node7);
+        graph.addEdge(node6, node8);
+        graph.addEdge(node4, node9);
+        graph.addEdge(node4, node10);
+        graph.addEdge(node4, node11);
+        graph.addEdge(node11, node12);
+
+        return graph;
+    }
+
+    public void setAlgorithm(GraphAdapter adapter) {
+        final BuchheimWalkerConfiguration configuration = new BuchheimWalkerConfiguration.Builder()
+                .setSiblingSeparation(100)
+                .setLevelSeparation(300)
+                .setSubtreeSeparation(300)
+                .setOrientation(BuchheimWalkerConfiguration.ORIENTATION_TOP_BOTTOM)
+                .build();
+        adapter.setAlgorithm(new BuchheimWalkerAlgorithm(configuration));
+    }
+
+    public class ViewHolder {
+        TextView textView;
+        ViewHolder(View view) {
+            textView = view.findViewById(R.id.textView);
+        }
+    }
+
+    protected String getNodeText() {
+        return "Node" + nodeCount++;
+    }
+}
